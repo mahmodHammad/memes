33bdf6101cd5c33bc7e213b9c75ccbc2271a90e4
= <hodaman2012@gmail.com>
2021-05-30 15:00:04 +0200

    represent holes with index

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const totalMemorySize=1000;
+const holes = [[10,40],[70,100],[260,40],[300,20],[600,100]]
+const memory = document.querySelector(".memory")
+memory.style.height= `${totalMemorySize}px`
+// alert(holes[0][0])
+for(let hole = 0; hole <holes.length ; hole+=1){
+    // alert(hole)
+    const holeBox = document.createElement("div")
+    holeBox.innerText="THIS IS hole"
+    holeBox.classList.add("hole")
+    holeBox.style.top = `${holes[hole][0]}px`
+    holeBox.style.height = `${holes[hole][1]}px`
+    const endSize =  document.createElement("span")
+    endSize.innerText =holes[hole][0] + holes[hole][1] 
+    endSize.classList.add("holeEndIndex")
+
+    holeBox.appendChild(endSize)
+    memory.appendChild(holeBox)
+}
+console.log("MEM",memory)
 /*
 Inputs:
 {totalMemorySize, holes:{startingAdr,size}}
